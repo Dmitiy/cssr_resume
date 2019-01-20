05e6d5b46a791947e14fa0660be41fbdc7b0e470
Dmitiy <shkarlat.dmitry@yandex.ru>
2019-01-20 12:53:56 +0300

    add pikaday, moment libs and underline to textarea

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,22 @@
 import autosize from 'autosize';
+import moment from 'moment';
+import Pikaday from 'Pikaday';
+import 'pikaday/css/pikaday.css';
 
 (() => {
     const block = document.querySelectorAll('.textarea');
 
     autosize(block);
     autosize(document.querySelector('textarea'));
+
+    const field = document.getElementById('datepicker');
+    const picker = new Pikaday({
+        format: 'DD.MM.YYYY',
+        field,
+        onSelect: function (date) {
+            field.value = picker.toString();
+            console.log(this.getMoment().format('DD.MM.YYYY'));
+        }
+    });
 
 })()
